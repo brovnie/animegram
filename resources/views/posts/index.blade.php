f184ea40a6c372177c043da6ba847845ef9db611
brovnie-ubuntu <maarl.deenis@gmail.com>
2023-02-25 19:14:53 +0100

    Created welcome message + loop through posts only if not empty fixes #1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,26 +2,35 @@
 
 @section('content')
 <div class="container">
-@foreach($posts as $post)
+@if($posts->isEmpty())
     <div class="row">
-        <div class="col-6 offset-3">
-            <a href="/profile/{{$post->user->id}}">
-                <img src="/storage/{{ $post->image }}" alt="" class="w-100">
-            </a>
+        <div>
+            <h2>Welcome to Animagram!</h2>
+            <p>There are no posts avialable at this moment.</p>
+            <p>Please follow the users to see the posts</p>
         </div>
     </div>
-    <div class="row pt-2 pb-4">
-        <div class="col-6 offset-3">
+@else
+    @foreach($posts as $post)
+        <div class="row">
+            <div class="col-6 offset-3">
+                <a href="/profile/{{$post->user->id}}">
+                    <img src="/storage/{{ $post->image }}" alt="" class="w-100">
+                </a>
+            </div>
+        </div>
+        <div class="row pt-2 pb-4">
+            <div class="col-6 offset-3">
 
-            <p><a href="/profile/{{$post->user->id}}" class="text-dark"><span class="font-weight-bold">{{ $post->user->username}}</span></a>
-             {{ $post->caption }} </p>
+                <p><a href="/profile/{{$post->user->id}}" class="text-dark"><span class="font-weight-bold">{{ $post->user->username}}</span></a>
+                {{ $post->caption }} </p>
+            </div>
         </div>
-    </div>
     @endforeach
     <div class="row">
     <div class="col-12 d-flex justify-content-center">
     {{$posts->links("pagination::bootstrap-4")}}
-
     </div></div>
+@endif
 </div>
 @endsection
